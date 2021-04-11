dcd65d55d2bd8f72fa64eab1daf906199f393169
Daniel Ockiya <annonbytes@gmail.com>
2021-04-11 20:59:12 +0100

    structure change

---
+++ recipe-app/src/config/config.ts
@@ -0,0 +1,13 @@
+const config = {
+    firebase: {
+        apiKey: "AIzaSyB4JE7yZ2XzKD0pE8CwM6CAZ4XOMT0dryE",
+        authDomain: "recipe-app-d9936.firebaseapp.com",
+        projectId: "recipe-app-d9936",
+        storageBucket: "recipe-app-d9936.appspot.com",
+        messagingSenderId: "681359373716",
+        appId: "1:681359373716:web:e2aa92efd47ccc45facb81",
+        measurementId: "G-9FXZJ9TKR5"
+    }
+}
+
+export default config
---
+++ recipe-app/src/config/firebase.ts
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import config from '../src/config/config'
+
+
+
+
+const firebaseApp = firebase.initializeApp(config.firebase)
+
+export const Providers = {
+  google: new firebase.auth.GoogleAuthProvider()
+}
+
+
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+
+export { db, auth }
