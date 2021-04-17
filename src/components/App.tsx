0fedafeebe01572dc76859592dd4e6f84584f29b
kaankotan <*Yarak31*>
2021-04-17 18:39:48 +0200

    Before ready for merge, Search.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,32 +5,38 @@ import Signup from "./Signup";
 import Dashboard from "./Dashboard"
 import Login from './Login'
 import Search from './Search'
+import AddRecipe from './AddRecipe'
+import { useSelector, useDispatch } from 'react-redux'
+import './app-style.css';
 // @ts-ignore
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import PrivateRoute from './PrivateRoute'
 import ForgotPassword from './ForgotPassword'
-import './app-style.css';
+import CustomNavbar from './CustomNavbar';
 
 function App() {
   return (
-    <Container className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}
-    >
-      <div className="w-100" style={{ maxWidth: "400px" }}>
-        <Router>
-          <AuthProvider>
-            <Switch>
-              <PrivateRoute exact path="/" component={Dashboard} />
-              <Route path="/signup" component={Signup} />
-              <Route path="/login" component={Login} />
-              <Route path="/forgot-password" component={ForgotPassword} />
-              <Route path="/search" component={Search} />
-            </Switch>
-          </AuthProvider>
-        </Router>
+      <div>
+        <CustomNavbar />
+        <Container className="d-flex align-items-center justify-content-center"
+                   style={{ minHeight: "100vh" }}
+        >
+          <div className="w-100" style={{ maxWidth: "400px" }}>
+            <Router>
+              <AuthProvider>
+                <Switch>
+                  <PrivateRoute exact path="/" component={Dashboard} />
+                  <PrivateRoute path="/add-recipe" component={AddRecipe} />
+                  <Route path="/search" component={Search} />
+                  <Route path="/signup" component={Signup} />
+                  <Route path="/login" component={Login} />
+                  <Route path="/forgot-password" component={ForgotPassword} />
+                </Switch>
+              </AuthProvider>
+            </Router>
+          </div>
+        </Container>
       </div>
-    </Container>
-
   )
 }
 
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,19 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Card, Button, Alert } from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext'
+import firebase from 'firebase';
 // @ts-ignore
 import { Link, useHistory } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import {NumberState} from "../types";
+import RecipeList from './RecipeList';
 // it is like useAuth Hook?
-
-
 
 export default function Dashboard() {
   const [error, setError] = useState('')
   const { currentUser, logout } = useAuth()
   const { history } = useHistory()
 
-  
+  const amount = useSelector<NumberState, number>(state => state.someNumber)
+  const dispatch = useDispatch()
+
+  function incrementNumber() {
+    dispatch({type: "INCREMENT"})
+  }
+
+  function decrementNumber() {
+    dispatch({type: "DECREMENT"})
+  }
 
   async function handleLogout() {
     setError('')
@@ -27,24 +37,30 @@ export default function Dashboard() {
   }
 
   return (
-    <>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-3">Profile</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <strong>Email:</strong> {currentUser.email}
-          <Link to="/update-profile" className="btn btn-primary w-100 mt-3 shadow">Update Profile</Link>
-          <Link to="/search" className="btn btn-primary w-100 mt-3 shadow">Search Recipe</Link>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        <Button variant="link" onClick={handleLogout}>
-          Log Out!
-        </Button>
-      </div>
-
-      
-    </>
+      <>
+        <RecipeList />
+        <Card>
+          <Card.Body className="d-flex flex-row">
+            <Button onClick={decrementNumber} >-</Button>
+            <h2>{amount}</h2>
+            <Button onClick={incrementNumber} >+</Button>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Body>
+            <h2 className="text-center mb-3">Profile</h2>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <strong>Email:</strong> {currentUser.email}
+            <Link to="/update-profile" className="btn btn-primary w-100 mt-3">Update Profile</Link>
+          </Card.Body>
+        </Card>
+        <div className="w-100 text-center mt-2">
+          <Button variant="link" onClick={handleLogout}>
+            Log Out!
+          </Button>
+        </div>
+        <Link to="/add-recipe" className="btn btn-primary w-100 mt-3">Add Recipe</Link>
+        <Link to="/search" className="btn btn-primary w-100 mt-3 shadow">Search Recipe</Link>
+      </>
   )
-  
 }
