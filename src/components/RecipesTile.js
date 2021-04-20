d294f420d52ac74bde941797c83cc9f778f2e247
Mojtaba-Zahedi-Amiri <mojtaba.zahedi.amiri@gmail.com>
2021-04-20 23:06:41 +0200

    details in research page

--- src/components/RecipesTile.js
+++ src/components/RecipesTile.js
@@ -7,6 +7,8 @@ export default function RecipesTile({recipe}) {
         <div className="recipeTile" onClick={()=>{window.open(recipe["recipe"]["url"])}}>
             <img className="recipeTile__img" src={recipe["recipe"]["image"]}/>
             <p className="recipeTile__name ">{recipe["recipe"]["label"]}</p>
+            <p className="recipeTile__details ">cuisine : {recipe["recipe"]["cuisineType"]}</p>
+            <p className="recipeTile__details "> {recipe["recipe"]["mealType"]}</p>
         </div>
     )
 }
